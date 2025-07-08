128672f8d7354b0266ea74988cf5f2d5b90976b1
Anthony Stalin <138017403+ErenStalin@users.noreply.github.com>
2025-07-08 16:57:01 +0530

    Delete Monthly Revenue & Profit Trend.sql

--- Monthly Revenue & Profit Trend.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  DATE_TRUNC('month', date) AS month,
-  SUM(revenue) AS monthly_revenue,
-  SUM(cost) AS monthly_cost,
-  SUM(profit) AS monthly_profit
-FROM transactions
-GROUP BY DATE_TRUNC('month', date)
-ORDER BY month;
